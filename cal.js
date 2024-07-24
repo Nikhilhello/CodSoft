96f96241b75ec7cd976eb905215e7528fe34b901
Nikhilhello <kuchananikhil2004@gmail.com>
2024-07-24 14:44:30 +0530

    cal.js

--- cal.js
+++ cal.js
@@ -6,42 +6,55 @@ let num1 = "";
 let num2 = "";
 let displayOperation = "";
 
-buttons.forEach((button) => {
-  button.addEventListener("click", (e) => {
+buttons.forEach((button) => 
+  {
+  button.addEventListener("click", (e) => 
+    {
     let value = e.target.value;
 
-    if (value === "c") {
+    if (value === "c") 
+    {
       result.value = "";
       operator = "";
       num1 = "";
       num2 = "";
       displayOperation = "";
-    } else if (value === "=") {
-      if (num2 !== "") {
-        if (operator === "+") {
+    } else if (value === "=") 
+    {
+      if (num2 !== "") 
+      {
+        if (operator === "+") 
+        {
           result.value = parseFloat(num1) + parseFloat(num2);
-        } else if (operator === "-") {
+        } else if (operator === "-") 
+        {
           result.value = parseFloat(num1) - parseFloat(num2);
-        } else if (operator === "*") {
+        } else if (operator === "*") 
+        {
           result.value = parseFloat(num1) * parseFloat(num2);
-        } else if (operator === "/") {
+        } else if (operator === "/") 
+        {
           result.value = parseFloat(num1) / parseFloat(num2);
         }
         num1 = "";
         num2 = "";
         operator = "";
         displayOperation = "";
-      } else {
+      } else
+      {
         result.value = "Error";
       }
-    } else if (value === "+" || value === "-" || value === "*" || value === "/") {
+    } else if (value === "+" || value === "-" || value === "*" || value === "/")
+    {
       operator = value;
       num1 = result.value;
       result.value = "";
       displayOperation = num1 + " " + operator + " ";
-    } else {
+    } else 
+    {
       result.value += value;
-      if (operator !== "") {
+      if (operator !== "") 
+      
         num2 = result.value.replace(displayOperation, "");
         result.value = displayOperation + num2;
       }
